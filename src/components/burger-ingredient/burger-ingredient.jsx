ee1f6c65c2fa216a96b7ef8cef3cb631df8ef24b
Александр Васянькин <henchfiend@yandex.ru>
2023-05-13 20:36:22 +0300

    список ингредиентов ч2
    - добавлен отдельный компонент создающий карточку
    - добавлены стили шрифтов
    - размеры элементов приведены в соответствие с макетом
    - карточки ингредиентов организованы в плитку
    - счётчик ингредиентов верно спозиционирован

---
+++ src/components/burger-ingredient/burger-ingredient.jsx
@@ -0,0 +1,21 @@
+import {
+    Counter,
+    CurrencyIcon
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './burger-ingredient.module.css';
+
+const BurgerIngredient = ({ingredient}) => {
+  return (
+    <div className={styles.card}>
+      <Counter className={styles.counter}/>
+      <img src={ingredient.image} alt={ingredient.name} className={styles.image} />
+      <div className="#">
+        <span className="text text_type_digits-default">{ingredient.price}</span>
+        <CurrencyIcon type="primary" />
+      </div>
+      <h3 className="text text_type_main-default">{ingredient.name}</h3>
+    </div>
+  );
+};
+
+export default BurgerIngredient
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,38 +1,40 @@
-import React from 'react';
 import {
-    Counter,
-    CurrencyIcon,
     Tab
 } from '@ya.praktikum/react-developer-burger-ui-components';
+import BurgerIngredient from '../burger-ingredient/burger-ingredient';
 import styles from './burger-ingredients.module.css';
 
 const BurgerIngredients = ({ data }) => {
   return (
-    <section className="#">
+    <section className={styles[`ingredients-list`]}>
       <h1 className="text text_type_main-large">Соберите бургер</h1>
       <div style={{ display: "flex" }}>
         <h2>Переключатель тут</h2>
       </div>
-      <div className="#">
-        <h2 className="text text_type_main-default">Булки</h2>
+      <h2 className="text text_type_main-large">Булки</h2>
+      <div className={styles[`grid-container`]}>
         {data.map((item) => {
-          return (
-            <div className="#">
-              <Counter />
-              <img src={item.image} alt={item.name} className="#" />
-              <div>
-                <span className="text text_type_digits-default">
-                  {item.price}
-                </span>
-                <CurrencyIcon type="primary" />
-              </div>
-              <h3>{item.name}</h3>
-            </div>
-          );
+          if (item.type === "bun") {
+            return <BurgerIngredient ingredient={item} />;
+          } else return null;
         })}
       </div>
-      <h2 className="text text_type_main-default">Соусы</h2>
-      <h2 className="text text_type_main-default">Начинки</h2>
+      <h2 className="text text_type_main-large">Соусы</h2>
+      <div className={styles[`grid-container`]}>
+        {data.map((item) => {
+          if (item.type === "sauce") {
+            return <BurgerIngredient ingredient={item} />;
+          } else return null;
+        })}
+      </div>
+      <h2 className="text text_type_main-large">Начинки</h2>
+      <div className={styles[`grid-container`]}>
+        {data.map((item) => {
+          if (item.type === "main") {
+            return <BurgerIngredient ingredient={item} />;
+          } else return null;
+        })}
+      </div>
     </section>
   );
 };
