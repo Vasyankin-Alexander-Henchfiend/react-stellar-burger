454e4c696836d448885aed72124c87effd4b27a4
yndx-handbook <henchfiend@yandex.ru>
2023-07-01 01:32:46 +0300

    подключение API
    - данные всех ингредиентов теперь берутся с сервера через API
    - убран инлайновый стиль в компоненте APP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,17 +1,33 @@
+import React, {useEffect} from "react";
 import styles from "./app.module.css";
-import { data } from "../../utils/data";
 import { constructorData } from '../../utils/constructor-data';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 
+const URL = 'https://norma.nomoreparties.space/api/ingredients';
+
+
 function App() {
+
+  const[data, setData] = React.useState([])
+
+  useEffect(() => {
+    const getData = async () => {
+    try {
+        const res = await fetch(URL);
+        const item = await res.json();
+        return setData(item.data);
+      } catch (e) {
+        return console.log(`'Что-то пошло не так ${e}'`);
+      }
+    }
+    getData()
+  }, [])
+
   return (
     <div className={styles.app}>
-      <pre style={{
-        margin: 0,
-      	fontSize: "1.5rem"
-      }}>
+      <pre className={styles.pre}>
       	<AppHeader  />
         <main className={styles.main}>
           <BurgerIngredients data={data}/>
